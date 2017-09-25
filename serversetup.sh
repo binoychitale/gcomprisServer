5c9ab414d71bc51e5d5a7ecc63d487bfd56d92d4
binoychitale <binoychitale@gmail.com>
2017-09-25 18:11:01 +0000

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -7,6 +7,7 @@ sudo apt-get install gcompris
 mkdir ${HOME}/Desktop/gcserverconfig
 #back up original samba config file
 mkdir ${HOME}/gcbackups/
+sudo chmod -R 777 /etc/samba/smb.conf
 sudo cp /etc/samba/smb.conf ${HOME}/gcbackups/
 sudo echo "[gcserverconfig]
 path = ${HOME}/gcserverconfig
