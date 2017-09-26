54f95c49efef196a52a0b86f33258aa105d56f9e
binoychitale <binoychitale@gmail.com>
2017-09-26 03:39:42 +0000

    Update clientsetup.sh

--- clientsetup.sh
+++ clientsetup.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install gcompris
 sudo apt-get install smbclient
-sudo smbclient //${1}/gcserverconfig -U $USER --no-pass
 mkdir ${HOME}/Desktop/remoteserver
-sudo mount -t cifs //${1}/gcserverconfig ${HOME}/Desktop/remoteserver -o username=$USER guest
+sudo mount -t cifs //$1/gcserverconfig/ ${HOME}/Desktop/remoteserver -o username=$USER
+echo "alias gcomprisremote='gcompris --database /home/pi/Desktop/remoteserver/gcompris_sqlite.db --config-dir ${HOME}/Desktop/remoteserver/ --profile=TESTPROF'
+alias gcadmin='gcompris --administration --database /home/pi/Desktop/remoteserver/gcompris_sqlite.db'" > ${HOME}/.bash_aliases
