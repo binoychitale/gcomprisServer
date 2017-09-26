f93cabd0bec8fbeb270a377cffd72ed4245b650b
binoychitale <binoychitale@gmail.com>
2017-09-26 03:45:40 +0000

    Update clientsetup.sh

--- clientsetup.sh
+++ clientsetup.sh
@@ -8,3 +8,4 @@ mkdir ${HOME}/Desktop/remoteserver
 sudo mount -t cifs //$1/gcserverconfig/ ${HOME}/Desktop/remoteserver -o username=$USER
 echo "alias gcomprisremote='gcompris --database /home/pi/Desktop/remoteserver/gcompris_sqlite.db --config-dir ${HOME}/Desktop/remoteserver/ --profile=TESTPROF'
 alias gcadmin='gcompris --administration --database /home/pi/Desktop/remoteserver/gcompris_sqlite.db'" > ${HOME}/.bash_aliases
+. ~/.bashrc
